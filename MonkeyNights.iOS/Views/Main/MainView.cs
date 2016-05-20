a91a9b689f266c786ba1ab1ce88210bbe786c9c0
William S. Rodriguez <williamsrz@outlook.com>
2016-05-20 15:55:35 -0300

    added iOS project bootstrap

---
+++ MonkeyNights.iOS/Views/Main/MainView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using UIKit;
+using MonkeyNights.Core.ViewModels;
+using MvvmCross.Binding.BindingContext;
+
+namespace MonkeyNights.iOS
+{
+    public partial class MainView : BaseView
+    {
+        public MainView ()
+            : base ("MainView", null)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            var set = this.CreateBindingSet<MainView, MainViewModel> ();
+        }
+    }
+}
+
+
