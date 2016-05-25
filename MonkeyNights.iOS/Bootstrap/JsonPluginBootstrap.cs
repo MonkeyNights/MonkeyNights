7f5e4d9d80e40fd13e597e1e32895c288a551431
William S. Rodriguez <williamsrz@outlook.com>
2016-05-25 16:51:16 -0300

    WTF is this.

--- MonkeyNights.iOS/Bootstrap/JsonPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace MonkeyNights.iOS.Bootstrap
-{
-    public class JsonPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
-    {
-    }
-}
